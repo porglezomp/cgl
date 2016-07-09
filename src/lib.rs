2e64c4a42279900414fb05468b841c089efc897c
Caleb Jones <code@calebjones.net>
2016-07-09 02:48:10 -0400

    Implement Bresenham's Line Algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -45,22 +45,59 @@ impl Image {
             height: height,
         }
     }
+
+    /// Draw a line into the image using Bresenham's Line Drawing Algorithm
+    pub fn line(&mut self, mut x0: isize, mut y0: isize,
+                mut x1: isize, mut y1: isize, color: Color)
+    {
+        let steep = (x0 - x1).abs() < (y0 - y1).abs();
+        if steep {
+            std::mem::swap(&mut x0, &mut y0);
+            std::mem::swap(&mut x1, &mut y1);
+        }
+        if x0 > x1 {
+            std::mem::swap(&mut x0, &mut x1);
+            std::mem::swap(&mut y0, &mut y1);
+        }
+        let (x0, y0, x1, y1) = (x0, y0, x1, y1);
+        let (dx, dy) = (x1 - x0, y1 - y0);
+        let derror = dy.abs()*2;
+        let yoff = if y1 > y0 { 1 } else { -1 };
+        let mut error = 0;
+        let mut y = y0;
+
+        for x in (x0..x1).chain(Some(x1)) {
+            if 0 <= x && x < self.width as isize &&
+                    0 <= y && y < self.height as isize {
+                if steep {
+                    self[(y as usize, x as usize)] = color;
+                } else {
+                    self[(x as usize, y as usize)] = color;
+                }
+            }
+            error += derror;
+            if error > dx {
+                y += yoff;
+                error -= dx*2;
+            }
+        }
+    }
 }
 
 impl Index<(usize, usize)> for Image {
     type Output = Color;
-    fn index(&self, (row, col): (usize, usize)) -> &Self::Output {
-        assert!(row < self.width);
-        assert!(col < self.height);
-        &self.pixels[row * self.width + col]
+    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
+        assert!(x < self.width);
+        assert!(y < self.height);
+        &self.pixels[y * self.width + x]
     }
 }
 
 impl IndexMut<(usize, usize)> for Image {
-    fn index_mut(&mut self, (row, col): (usize, usize)) -> &mut Self::Output {
-        assert!(row < self.width);
-        assert!(col < self.height);
-        &mut self.pixels[row* self.width + col]
+    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
+        assert!(x < self.width);
+        assert!(y < self.height);
+        &mut self.pixels[y * self.width + x]
     }
 }
 
@@ -91,5 +128,30 @@ mod tests {
         let mut im = Image::with_dimensions(4, 4);
         im[(0, 0)] = Color::white();
         assert_eq!(im[(0, 0)], Color::white());
+    }
+
+    #[test]
+    fn draw_line() {
+        let mut im = Image::with_dimensions(4, 4);
+        im.line(0, 0, 3, 0, Color::white());
+        for x in 0..4 {
+            assert_eq!(im[(x, 0)], Color::white());
+            assert_eq!(im[(x, 1)], Color::black());
+        }
+
+        let mut im = Image::with_dimensions(4, 4);
+        im.line(0, 0, 3, 3, Color::blue());
+        for i in 0..4 {
+            assert_eq!(im[(i, i)], Color::blue());
+        }
+        assert_eq!(im[(3, 0)], Color::black());
+        assert_eq!(im[(0, 3)], Color::black());
+
+        let mut im = Image::with_dimensions(4, 4);
+        im.line(0, 0, 0, 3, Color::red());
+        for y in 0..4 {
+            assert_eq!(im[(0, y)], Color::red());
+            assert_eq!(im[(1, y)], Color::black())
+        }
     }
 }
