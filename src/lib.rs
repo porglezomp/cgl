c433a5cf0d026fc9c1022be1fca8e7fa5f91f2c0
Caleb Jones <code@calebjones.net>
2016-07-09 01:41:12 -0400

    Add Color and Image
    The Image type holds an array of pixels as well as the width and height
    of that array.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,54 @@
+/// A type representing an RGB triple
+#[derive(Debug, Default, PartialEq, Eq, Clone, Copy)]
+pub struct Color(pub u8, pub u8, pub u8);
+
+/// Stores an image on the heap for drawing into
+#[derive(Clone)]
+pub struct Image {
+    pixels: Vec<Color>,
+    pub width: usize,
+    pub height: usize,
+}
+
+impl Image {
+    /// Creates a new `width` by `height` black `Image`.
+    pub fn with_dimensions(width: usize, height: usize) -> Self {
+        let mut pixels = Vec::with_capacity(width * height);
+        pixels.resize(width * height, Default::default());
+        Image {
+            pixels: pixels,
+            width: width,
+            height: height,
+        }
+    }
+
+    /// Create a new image containing the data in `pixels`
+    ///
+    /// # Panics
+    ///
+    /// Panics if `pixels` doesn't have `width * height` elements
+    pub fn with_pixels(width: usize, height: usize, pixels: &[Color]) -> Self {
+        assert_eq!(pixels.len(), width * height);
+        Image {
+            pixels: Vec::from(pixels),
+            width: width,
+            height: height,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::{Color, Image};
+
     #[test]
-    fn it_works() {
+    fn default_black() {
+        assert_eq!(<Color as Default>::default(), Color(0, 0, 0));
+    }
+
+    #[test]
+    #[should_panic]
+    fn incorrect_pixel_size() {
+        Image::with_pixels(4, 4, &[Default::default(); 4]);
     }
 }
