538219b6a411019d0bed5217f27e451cdd9499e8
Caleb Jones <code@calebjones.net>
2016-07-13 02:40:03 -0400

    Fix broken wireframe demo

--- examples/wireframe.rs
+++ examples/wireframe.rs
@@ -12,18 +12,20 @@ fn main() {
         .expect("should load model");
     let mut image = Image::with_dimensions(512, 512);
 
+    let scale = Vec3(256.0, -256.0, 1.0);
+    let offset = Vec3(256.0, 256.0, 0.0);
     for tri in model.triangles {
-        let t0 = model.vertices[tri[0] as usize] * 256.0 + Vec3(256.0, -256.0, 0.0);
-        let t1 = model.vertices[tri[1] as usize] * 256.0 + Vec3(256.0, -256.0, 0.0);
-        let t2 = model.vertices[tri[2] as usize] * 256.0 + Vec3(256.0, -256.0, 0.0);
+        let t0 = model.vertices[tri[0] as usize] * scale + offset;
+        let t1 = model.vertices[tri[1] as usize] * scale + offset;
+        let t2 = model.vertices[tri[2] as usize] * scale + offset;
 
         image.line(t0.0 as isize, t0.1 as isize, t1.0 as isize, t1.1 as isize, Color::white());
         image.line(t1.0 as isize, t1.1 as isize, t2.0 as isize, t2.1 as isize, Color::white());
         image.line(t2.0 as isize, t2.1 as isize, t0.0 as isize, t0.1 as isize, Color::white());
     }
 
-    let mut img_out = File::create("demo/demo.bmp")
-        .expect("should create demo.bmp");
+    let mut img_out = File::create("demo/demo001.bmp")
+        .expect("should create demo001.bmp");
     write_bmp(&image, &mut img_out)
         .expect("should save image");
 }
